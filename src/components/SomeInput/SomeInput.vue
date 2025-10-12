6dc29eabd078209ba4dbdfba371567bbc5dbf44a
Yaroslav Chaplinskyi <116737021+Yarzik7@users.noreply.github.com>
2025-10-12 13:29:28 +0300

    work with vuetify

---
+++ src/components/SomeInput/SomeInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="input-wrap">
+    <label :for="forId" class="label">{{ labelText }}</label>
+    <v-textarea v-if="type === 'textarea'" :value="value" :id="forId" class="v-input-wrap" @input="onInput" />
+    <v-text-field v-if="type === 'text'" :value="value" :id="forId" class="v-input-wrap" @input="onInput" />
+    <v-btn-toggle v-if="type === 'checkBtns'" v-model="ckeckBtnsVal" dense background-color="primary" dark multiple>
+      <v-btn v-for="(item, index) in checkBtnEntries" :key="index" @input="onCheckInput($event, item)">
+        {{ item[0] }}
+      </v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SomeInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    labelText: {
+      type: String,
+      default: 'Some input',
+    },
+    value: {
+      type: [String, Number, Array, Object],
+      default: [],
+    },
+    elRef: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      forId: crypto.randomUUID(),
+      inputValue: '',
+      ckeckBtnsVal: [],
+    };
+  },
+  computed: {
+    checkBtnEntries() {
+      return Object.entries(this.value);
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('input', event);
+    },
+    onCheckInput(event, item) {
+      this.$emit('input', { value: item[1], isChecked: event });
+    },
+  },
+};
+</script>
+<style scoped>
+.label {
+  display: block;
+  margin-bottom: 10px;
+}
+.v-input-wrap {
+  border: 1px solid #fff;
+  border-radius: 8px;
+  background-color: rgb(212, 212, 212);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-wrapper">
+    <v-form id="home-view-form" class="v-form-wrap" @submit.prevent="onSubmit">
+      <some-input :value="someMap" type="checkBtns" label-text="Chechbox buttons" @input="onThisFormCheckInput" />
+      <some-input
+        ref="someTextarea"
+        :value="template"
+        type="textarea"
+        label-text="Test textarea"
+        @input="onThisFormInput($event, 'template')"
+      />
+      <some-input :value="text" label-text="Test field" @input="onThisFormInput($event, 'text')" />
+    </v-form>
+    <div class="form-control-box">
+      <v-btn :form="'home-view-form'" type="submit" class="v-form-btn">Submit</v-btn>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SomeInput from '@/components/SomeInput/SomeInput.vue';
 
 export default {
-  name: 'HomeView',
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    SomeInput,
+  },
+  data: () => ({
+    someMap: [],
+    template: '',
+    text: '',
+  }),
+  mounted() {
+    this.getTemplate();
+    this.getSomeMap();
+  },
+  methods: {
+    getTemplate() {
+      this.template = `
+      line1:
+      line2:
+      line3:
+      `;
+    },
+    getSomeMap() {
+      this.someMap = { Key1: 'value_1', Key2: 'value_2', Key3: 'value_3' };
+    },
+    onSubmit() {
+      console.log('Submit');
+      console.log({ template: this.template, text: this.text });
+    },
+    onThisFormInput(event, field) {
+      if (!field) return;
+      this[field] = event;
+    },
+    onThisFormCheckInput({ value, isChecked }) {
+      const textareaEl = this.$refs.someTextarea?.$el.querySelector('textarea');
+      if (!textareaEl) return;
+
+      if (!isChecked) {
+        this.template = this.template.replace(value, '');
+        return;
+      }
+
+      const startCursorPosition = textareaEl.selectionStart;
+      const endCursorPosition = textareaEl.selectionEnd;
+
+      const beforeText = this.template.slice(0, startCursorPosition);
+      const afterText = this.template.slice(endCursorPosition);
+      const newTextareaValue = beforeText + value + afterText;
+
+      this.template = newTextareaValue;
+    },
+  },
+};
 </script>
+<style>
+.home-wrapper {
+  padding: 60px;
+}
+.v-form-wrap {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  margin-bottom: 25px;
+}
+.form-control-box {
+  display: flex;
+  align-items: center;
+  gap: 25px;
+}
+.v-form-btn {
+  margin-left: auto;
+}
+</style>
